8d399df8aa7c942e6ba48ff62341ed8c3fc2e7a0
abressiddique <abressiddique@gmail.com>
2024-08-08 02:19:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import Workout from './components/Workout'
 import { generateWorkout } from './utils/functions'
 
 
+
 function App() {
+  
   const [workout, setWorkout] = useState(null)
   const [poison, setPoison] = useState('individual')
   const [muscles, setMuscles] = useState([])
@@ -20,6 +22,7 @@ function App() {
 
     window.location.href = '#workout'
   }
+  /// 
 
   return (
     <main className='min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base'>
